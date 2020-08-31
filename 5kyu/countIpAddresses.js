2a11500fe14505074957b7a54feb8da314623056
Alexander Neupokoev <admin@armavir.ru>
2020-08-31 17:07:34 +0300

    Remove draft code

--- 5kyu/countIpAddresses.js
+++ 5kyu/countIpAddresses.js
@@ -4,12 +4,6 @@
 // All inputs will be valid IPv4 addresses in the form of strings.
 // The last address will always be greater than the first one.
 
-function countIP(ip) {
-    ip.split('.').forEach((octet, index) => {
-
-    })
-}
-
 function ipsBetween(start, end) {
     let startOctets = start.split('.')
     let endOctets = end.split('.')
@@ -19,3 +13,11 @@ function ipsBetween(start, end) {
 
 console.log(ipsBetween("20.0.0.10", "20.0.1.0"))
 
+// BEST Practice
+// function ipsBetween(start, end) {
+//     start = start.split('.');
+//
+//     return end.split('.').reduce(function(sum, x, i) {
+//         return (sum << 8) + Number(x) - Number(start[i])
+//     }, 0);
+// }
